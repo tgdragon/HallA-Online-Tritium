c059b132264953c51511c87ecf2462ac8fc78282
tgdragon <gogami.toshiyuki.4a@kyoto-u.ac.jp>
2019-12-05 10:49:59 +0900

    modified

--- replay_ole0/gen_maruhadaka_dat.py
+++ replay_ole0/gen_maruhadaka_dat.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 
-# Toshiyuki Gogami
-# December 5, 2019
+# This file will create dat file which is used for Rootfiles/maruhadaka.cc
+# 1. Replay data files using gogogo.py in which you specify *_replay.dat
+# 2. Run this script (*_replay.dat --> *.dat)
+# 3. Copy *.dat to Rootfiles/ to be used for Rootfiles/maruhadaka.cc 
+
+# --- Toshiyuki Gogami ----  #
+# --- December 5, 2019 ----  #
 
 import sys
 import time, os.path
@@ -20,6 +25,17 @@ target = "h2"
 runfile = target+"_replay.dat" 
 outfile = target+".dat"
 
+if target=="h2":
+    hypid = 1
+elif target=="h22":
+    hypid = 2
+elif target=="T2": 
+    hypid = 3
+elif target=="He3": 
+    hypid = 4
+else:
+    hypid = 27
+
 def main():
     inputfile = open(runfile,"r")
     lines = inputfile.readlines()
@@ -31,12 +47,22 @@ def main():
         
         inputfile2 = open("temptemp","r")
         lines2 = inputfile2.readlines()
+        fnum = 0
         for line2 in lines2:
             data2 = line2.rsplit(' ',1)
             length_data2 = len(data2)
             #print(data2[length_data2-1][-15:])
-            f.write(data2[length_data2-1])
-            print(data2[length_data2-1])
+            #f.write(data2[length_data2-1])
+            #print(data2[length_data2-1])
+            fnum = fnum +1
+        print(data[0],fnum)
+        
+        fnum_this = 0
+        for i in range(fnum):
+            content = data[0]+" "+str(fnum_this)+" "+str(hypid)+"\n"
+            f.write(content)
+            fnum_this = fnum_this + 1
+
     com2 = "rm -f temptemp"
     call (com2, shell=True)
         
